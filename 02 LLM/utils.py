53632aee5c850fff4619aec0516834e76fcc316d
kramstyles <kramstyles@outlook.com>
2026-07-31 01:30:50 -0700

    introduced to pipelines

--- 02 LLM/utils.py
+++ 02 LLM/utils.py
@@ -1,6 +1,10 @@
 import os
 
+from huggingface_hub import HfApi
 import tiktoken
+
+
+api = HfApi()
 
 
 def get_secret(name, default=None):
@@ -10,7 +14,6 @@ def get_secret(name, default=None):
         return config(name, default)
     except (ImportError, ModuleNotFoundError):
         return os.environ.get(name, default)
-
 
 
 def count_tokens(text: str, model_name: str = "gpt-4o") -> int:
@@ -31,3 +34,14 @@ def count_tokens(text: str, model_name: str = "gpt-4o") -> int:
     token_list = encoding.encode(text)
     return len(token_list)
 
+
+def get_hugging_model_size(model: str, revision=None):
+    # model = "distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+
+    model_info = api.model_info(repo_id=model, files_metadata=True, revision=revision)  # noqa:E501
+
+    # Calculate total size of all repo files
+    total_bytes = sum(file.size for file in model_info.siblings if file.size)
+    total_mb = total_bytes / (1024 * 1024)
+
+    print(f"Total download size: {total_mb:.2f} MB")
